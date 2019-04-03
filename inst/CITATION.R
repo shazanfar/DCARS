726e6964690dd51067fb8f5269156affb2a2386e
shazanfar <shazanfar@gmail.com>
2019-04-03 18:14:06 +0100

    add citation

---
+++ inst/CITATION.R
@@ -0,0 +1,17 @@
+note <- sprintf("R package version %s", meta$Version)
+
+bibentry(bibtype = "Manual",
+         title = "{DCARS}: Differential correlation across ranked samples",
+         author = c(person("Shila", "Ghazanfar"),
+                    person("Dario", "Strbenac"),
+                    person("John", "Ormerod"),
+                    person("Jean", "Yang"),
+                    person("Ellis", "Patrick")
+
+         ),
+         year = 2018,
+         journal = "Bioinformatics",
+         note = note,
+         url = "https://github.com/shazanfar/DCARS",
+         doi = "10.1093/bioinformatics/bty698",
+         header = "To cite scMerge in publications please use:")
